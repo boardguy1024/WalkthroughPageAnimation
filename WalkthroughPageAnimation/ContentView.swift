93ba1931fb842e9023d2db1b694b8d93864570a7
paku <boardguy1024@gmail.com>
2023-11-25 21:28:06 +0900

    Initial Commit

---
+++ WalkthroughPageAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WalkthroughPageAnimation
+//
+//  Created by paku on 2023/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WalkthroughPageAnimation/WalkthroughPageAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkthroughPageAnimationApp.swift
+//  WalkthroughPageAnimation
+//
+//  Created by paku on 2023/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WalkthroughPageAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
